b1afc640cf7b7c7b7e7bbd345c0224cf5f95d887
anryl050 <118693401+anryl050@users.noreply.github.com>
2023-02-04 20:48:43 -0800

    created page layout in HTML using BootStrap

---
+++ Assets/script.js
@@ -0,0 +1,31 @@
+// URL:  https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// resource: https://openweathermap.org/forecast5
+// resource: https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys
+
+var APIKey = "9606aff55a0bd3e2295b059c842b1f5d";
+
+// GIVEN a weather dashboard with form inputs
+
+//! WHEN I search for a city
+//! THEN I am presented with current and future conditions for that city and that city is added to the search history
+//TODO: need to fetch the information from the API, and display in the (1) console log and then (2) on the screen
+//TODO: need to write a function (for loop) to dyncamically create elements based and append them to the page to dsplay the information received from the API
+//TODO: need to store the infomation in the local storage for the searched city and display the city name in the Search history
+
+
+//! WHEN I view current weather conditions for that city
+//! THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+//TODO:
+//TODO:
+
+
+//! WHEN I view future weather conditions for that city
+//! THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+//TODO:
+//TODO:
+
+
+//! WHEN I click on a city in the search history
+//! THEN I am again presented with current and future conditions for that city
+//TODO:
+//TODO:
